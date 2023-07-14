5fa048d549a057aabf702835f85419528ca6a66f
YasminAliBBIT <Yasmin.Ali@strathmore.edu>
2023-07-14 20:04:08 +0300

    Created Receptionist dashboard

---
+++ app/Http/Controllers/ReceptionDash.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReceptionDash extends Controller
+{
+    public function Displaydashboard()
+    {
+        return view ('template');
+    }
+    
+}
